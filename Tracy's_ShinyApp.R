2fae3f1471daf60979854e9bc76c637090cc47e0
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2020-04-06 17:34:36 -0700

    new layers with buffer and without errors

--- Tracy's_ShinyApp.R
+++ Tracy's_ShinyApp.R
@@ -13,17 +13,26 @@ library(sf)
 # import data
 rec<- read.csv("data/all_receivers.csv")
 rel<- read.csv("data/releaseloc.csv")
-st_layers("data/hydro_forshiny")
+#st_layers("data/hydro_forshiny")
+st_layers("data/For_Pascale")
 
 #read in region distinctions 
-bypass <- st_read("data/hydro_forshiny","bypass_regions")
-sacr <- st_read("data/hydro_forshiny","SacRiv_NHDPlus")
-delta <- st_read("data/hydro_forshiny","Delta_waterways")
-sfb <- st_read("data/hydro_forshiny","SFBandSuisun")
-sfb <- st_transform(sfb,crs=4326)#puts in WGS84 CRS
+#bypass <- st_read("data/hydro_forshiny","bypass_regions")
+#sacr <- st_read("data/hydro_forshiny","SacRiv_NHDPlus")
+#delta <- st_read("data/hydro_forshiny","Delta_waterways")
+#sfb <- st_read("data/hydro_forshiny","SFBandSuisun")
+#sfb <- st_transform(sfb,crs=4326)#puts in WGS84 CRS
+
+buffer<-st_read("data/For_Pascale/Buffer_6km_MergeCopy.shp")
+buffer_4326 <- buffer %>% st_transform(crs = 4326)#puts in WGS84 CRS
+hydro<-st_read("data/For_Pascale/Hydro_Poly_Join.shp")
+hydro_4326 <- hydro %>% st_transform(crs = 4326)#puts in WGS84 CRS
+bypass<-st_read("data/For_Pascale/bypass_regions.shp")
+bypass_4326 <- bypass %>% st_transform(crs = 4326)#puts in WGS84 CRS
+
 
 myCategoryColor_function <- colorFactor("Dark2", rec$Type) 
-factpal <- colorFactor(topo.colors(5), sacr$River_Seg) # color palette for river segments according to Johnson et al 2017
+pal <- colorFactor(palette = "Blues", domain = hydro_4326$Region)
 
 #ui
 ui <- bootstrapPage(
@@ -56,7 +65,7 @@ fishIcon <- makeIcon(
 #custom info
 content <- paste(sep = "<br/>",
                  "Mapped Release and Receiver Locations:",
-                 "Visualize the number of recievers with optional Year filter.",
+                 "Visualize the number of recievers with Year filter.",
                  "Click on fish symbols for additional release information.")
 
 #server
@@ -73,9 +82,9 @@ server <- function(input, output) {
                 values = rec$Type, opacity = .8, 
                 position="topleft") %>%
       # add legend for river segments
-      addLegend(title = "River Regions", 
-                pal =  factpal,
-                values = sacr$River_Seg, opacity = .8, 
+      addLegend(title = "River Regions from Johnson et al 2017", 
+                pal =  pal,
+                values = hydro_4326$Region, opacity = .8, 
                 position="topleft")%>% 
       setView(lat =     38.63700, lng = -121.2210, zoom = 7)%>%
       addMeasure(
@@ -91,17 +100,19 @@ server <- function(input, output) {
   observe({
     recf<- filteredData()
     if(nrow(recf) != 0){
-      
-      leafletProxy("map", data = filteredData()) %>% 
-        addPolygons(data= bypass,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5) %>% # adds bypass
-        addPolygons(data= sacr,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5) %>% # sac river
-        addPolygons(data= delta,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5) %>% # delta 
-        addPolygons(data= sfb,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5)%>% # adds sfb and suisun
-        
+      leafletProxy("map", data = filteredData()) %>%
+        addPolygons(data = bypass_4326, 
+                    fillColor = ~pal(Region),
+                    color = ~pal(Region),
+                    weight = 1)%>%
+        addPolygons(data = buffer_4326, 
+                    fillColor = ~pal(Region),
+                    color = ~pal(Region),
+                    weight = 1)%>%
+        addPolygons(data = hydro_4326, 
+                    fillColor = ~pal(Region),
+                    color = ~pal(Region),
+                    weight = 1)%>%
         clearMarkers() %>% 
         addCircleMarkers(popup= paste("Location: ",recf$Location#,"<br>",
                                       #"River km:", recf$Rkm #river km in popup (but some are out-dated/inconsistent)
@@ -116,23 +127,24 @@ server <- function(input, output) {
   #  output$my_rows <- renderPrint({ filteredData() %>% nrow() })#shows how many rows are displayed on map
   observe({
     relf<- filteredData2()
-    if(nrow(relf) != 0){
-      
-      leafletProxy("map", data = filteredData2()) %>% 
-        addPolygons(data= bypass,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5) %>% # adds bypass
-        addPolygons(data= sacr,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5) %>% # sac river
-        addPolygons(data= delta,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5) %>% # delta 
-        addPolygons(data= sfb,color = ~factpal(River_Seg), weight = 1, smoothFactor = 0.5,
-                    opacity = 1.0, fillOpacity = 0.5)%>% # adds sfb and suisun
-        
+    if(nrow(relf) != 0){leafletProxy("map", data = filteredData2()) %>%
+        addPolygons(data = bypass_4326, 
+                    fillColor = ~pal(Region),
+                    color = ~pal(Region),
+                    weight = 1)%>%
+        addPolygons(data = buffer_4326, 
+                    fillColor = ~pal(Region),
+                    color = ~pal(Region),
+                    weight = 1)%>%
+        addPolygons(data = hydro_4326, 
+                    fillColor = ~pal(Region),
+                    color = ~pal(Region),
+                    weight = 1)%>%
         #release locations
         addMarkers(data=relf,icon=fishIcon, popup= paste("Release Location: ",rel$Release.Location,"<br>",
                                           "Study: ", relf$Study,"<br>",
                                           "Year: ",relf$Year,"<br>",
-                                          "Run: ",relf$Run),) #add number of fish released each year/location? 
+                                          "Run: ",relf$Run)) #add number of fish released each year/location? 
     }
     else{
       leafletProxy("map", data = relf) %>% 
