5b6fd438571dee83e91524ec7831a936c0efa846
Pascale Goertler <pascalegoertler@Pascales-MacBook-Air.local>
2020-05-04 16:25:21 -0700

    dat_17up results = 5

--- DataSorting4DFA.R
+++ DataSorting4DFA.R
@@ -25,7 +25,7 @@ NameList<-unique(grps_17td$FishID)
 NameList_v2 = gsub('-', '.', NameList)
 dat_17td <-dat.17[,colnames(dat.17)%in%NameList] #99
 
-# was supposed to loose 100 fish with latest QC... need to check NAs
+# I was supposed to loose 100 fish with latest QC... need to check NAs
 sapply(dat_17up, function(x)all(is.na(x)))
 sapply(dat_17md, function(x)all(is.na(x)))
 sapply(dat_17td, function(x)all(is.na(x))) # there is something wrong here - all Mok2017### are gone now
@@ -34,8 +34,8 @@ dat_17md = as.data.frame(sapply(dat_17md, as.numeric))
 dat_17up = as.data.frame(sapply(dat_17up, as.numeric))
 dat_17td = as.data.frame(sapply(dat_17td, as.numeric))
 
-dat_17md<-dat_17md[rowSums(is.na(dat_17md)) != ncol(dat_17md), ]
-dat_17up<-dat_17up[rowSums(is.na(dat_17up)) != ncol(dat_17up), ]
+dat_17md<-dat_17md[rowSums(is.na(dat_17md)) != ncol(dat_17md), ] #55
+dat_17up<-dat_17up[rowSums(is.na(dat_17up)) != ncol(dat_17up), ] #122
 dat_17td<-dat_17td[rowSums(is.na(dat_17td)) != ncol(dat_17td), ]
 
 dat.t<-t(dat_17md)
@@ -47,8 +47,8 @@ y.bar = apply(dat.t, 1, mean, na.rm=TRUE)
 dat.z = (dat.t - y.bar) * (1/Sigma)
 rownames(dat.z)=rownames(dat.t)
 
-mod1<-runDFA(obs=dat.z,NumStates=1,ErrStruc='DE') #
-mod2<-runDFA(obs=dat.z,NumStates=2,ErrStruc='DE') #
-mod3<-runDFA(obs=dat.z,NumStates=3,ErrStruc='DE') #
-mod4<-runDFA(obs=dat.z,NumStates=4,ErrStruc='DE') #
-mod5<-runDFA(obs=dat.z,NumStates=5,ErrStruc='DE') #
+mod1<-runDFA(obs=dat.z,NumStates=1,ErrStruc='DE') #33679.27
+mod2<-runDFA(obs=dat.z,NumStates=2,ErrStruc='DE') #31824.12
+mod3<-runDFA(obs=dat.z,NumStates=3,ErrStruc='DE') #30530.39
+mod4<-runDFA(obs=dat.z,NumStates=4,ErrStruc='DE') #29372.04
+mod5<-runDFA(obs=dat.z,NumStates=5,ErrStruc='DE') #28123.34
