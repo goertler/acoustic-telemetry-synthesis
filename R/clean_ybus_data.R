7b4891cf5515a7be44d894b476a78003c9bc3d03
Myfanwy <myfanwy.johnston@fishsciences.net>
2022-04-15 12:35:22 -0700

    add ybus data and cleaning script

--- R/clean_ybus_data.R
+++ R/clean_ybus_data.R
@@ -4,179 +4,69 @@
 library(lubridate)
 library(stringr)
 library(dplyr)
-
+source("R/utils.R")
 
 #TODO: # Fri Apr  8 15:35:28 2022 ------------------------------
-#  - get release datetimes from relGEN, add to ybus
-#  - get fishID from fishID_key, add to ybus
-# - make release detection data frame, join with ybus
-#  - keep only the ybus fish that are in fishID_key - that will keep only fish with >= 2 detections & that are detected at Chipps 
-#  - check that all GENS are in distance matrix 
 #  - save clean ybus data
 #  - bring in fishID key to clean_yoloace and remake results
 
 ybus = read.csv("data/YBUS/ybus_detections.csv", stringsAsFactors = FALSE)
 ybus = dplyr::rename(ybus, TagID = Transmitter) 
-relGEN = readxl::read_excel("data/YBUS/ybusTagIDs.xlsx") # has relGEN
-ybus = merge(ybus, relGEN[ , c("TagID", "RelLoc", "time")], by = "TagID", all.x = TRUE) # keep all the fish from ybus, but not relGEN, because that's ALL the fish
-
+relgen = readxl::read_excel("data/YBUS/ybusTagIDs.xlsx") # has relGEN
 fishID = read.csv("data/common_data/FishID_key.csv") # has fishID
-ybus = merge(ybus, fishID[ , c("TagID", "FishID")], by = "TagID", all.x = TRUE)
+fishID = subset(fishID, TagType == "Vemco" & Year == 2016)
 
-dist_ybus = read.csv("data/distance_matrices/Distance_Matrix_YBUS_corr.csv")
-dist_ybus = select(dist_ybus, Name = Name_corr, Total_Length_m = Total_Length)
+relgen$DateTimePST = as.POSIXct(paste(as.character(relgen$`RELEASE DATE`), 
+                                  relgen$`RELEASE TIME`, sep = " "), 
+                            format = "%Y-%m-%d %H%M",
+                            tz = "Etc/GMT+8")
 
-## Merge pertinent info
+relgen$GEN = relgen$`RELEASE LOCATION`
+relgen$TagID = relgen$`TAG ID`
+
+# filter relgen down to only the fish in fishID key and vice-versa
+fish = relgen[ , c("TagID", "DateTimePST", "GEN")]
+
+stopifnot(all(!colSums(is.na(fish)))) # Says stop if it's not true that all of the column sums are 0 (false).   all exits as soon as it sees a single false;
+# any exits as soon as it sees a single true.  They're high-performance because you don't need to calculate the entire vector. 
+sum(unique(ybus$TagID) %in% fish$TagID)
+setdiff(ybus$TagID, fish$TagID) # 39131 is missing from FishID_key; this one's track is backwards, gets excluded
+
 ybus$DateTimePST <- as.POSIXct(ybus$DateTime, format = "%Y-%m-%d %H:%M:%S", tz="Etc/GMT+8")
-ybus$RelTime = ymd_hms(ybus$time, tz = "Etc/GMT+8")
 
-# make release detections df
-reldet = select(ybus, FishID, DateTimePST = RelTime)
-reldet$GEN = "Release"
-reldet %>% 
-  group_by(FishID) %>% 
-  filter(DateTimePST == min(DateTimePST)) -> reldet
-reldet = as.data.frame(reldet)
+tmp = rbind(fish, 
+            ybus[ , c("TagID", "DateTimePST", "GEN")])
+str(tmp)
 
-keep = c("FishID", "DateTimePST", "GEN")
-ybus = ybus[ , keep]
+ybus2 = merge(tmp, fishID[ , c("TagID", "FishID")], by = "TagID", all = FALSE)
 
-ybus = rbind(ybus, reldet)
+stopifnot(all(!colSums(is.na(ybus2))))
 
+dist_ybus = read.csv("data/distance_matrices/Distance_Matrix_YBUS_corr_07_21.csv")
+dist_ybus = select(dist_ybus, Name = Name_corr, Total_Length_m = Total_Length, Location)
 
-## Now fix some GEN loc names that don't  match in detection files vs dist matrices
-ybus[which(ybus$GEN == "3A"|ybus$GEN == "3B"|ybus$GEN == "3C"|ybus$GEN == "3D"),"GEN"] <- "Hood"
-ybus[which(ybus$GEN == "MAL.2"|
-             ybus$GEN == "MAL.7b"|ybus$GEN == "MAL.11a"|ybus$GEN == "MAL.6"|
-             ybus$GEN == "MAL.10b"|ybus$GEN == "MAL.8b"|ybus$GEN == "MAL.4"|
-             ybus$GEN == "MAL.14"|ybus$GEN == "MAL.7a"|ybus$GEN == "MAL.8a"|
-             ybus$GEN == "MAL.1"|ybus$GEN == "MAL.13"|ybus$GEN == "MAL.17"|
-             ybus$GEN == "MAL.11b"|ybus$GEN == "MAL.5"|ybus$GEN == "MAL.12a"|
-             ybus$GEN == "MAL.18"|ybus$GEN == "MAL.10a"|ybus$GEN == "MAL.12b"),"GEN"] <- "ChippsE"
-ybus[which(ybus$GEN == "Below Lisbon Weir"),"GEN"] <- "YB_LisbonWeir"
-ybus[which(ybus$GEN == "Base of Toe Drain"),"GEN"] <- "ToeDrainBase"
-ybus[which(ybus$GEN == "CCH.2"|ybus$GEN == "CCH.1"|
-             ybus$GEN == "Cache Slough East 1"|ybus$GEN == "Cache Slough East 2"|
-             ybus$GEN == "Cache Slough West 1"|ybus$GEN == "Cache Slough West 2"),"GEN"] <- "YB_LibertyIsBase"
-ybus[which(ybus$GEN == "GEO.u"|ybus$GEN == "GEO.d"),"GEN"] <- "Georgiana_Slough"
-ybus[which(ybus$GEN == "BSBGU.2"|ybus$GEN =="SBGD.2"),"GEN"] <- "BlwGeorgiana"
-ybus[which(ybus$GEN == "MOK.u"|ybus$GEN == "MOK.d"),"GEN"] <- "SouthofBW"
-ybus[which(ybus$GEN == "SACD.1"|ybus$GEN == "SACD.2"|ybus$GEN == "SACD.3"|
-             ybus$GEN == "SACD.4"),"GEN"] <- "SR_Mouth"
-ybus[which(ybus$GEN == "SACU.1"|ybus$GEN == "SACU.2"|
-             ybus$GEN == "SACU.3"|ybus$GEN == "SACU.4"),"GEN"] <- "Blw_Steamboat"
-ybus[which(ybus$GEN == "STMU.1"|ybus$GEN=="STMU.2"),"GEN"] <- "SteamboatSlough"
-ybus[which(ybus$GEN == "STMD.1"|ybus$GEN == "STMD.2"|
-             ybus$GEN == "STMD.3"|ybus$GEN == "STMD.4"),"GEN"] <- "BaseSutterSteam"
-ybus[which(ybus$GEN == "RSTR (Rotary Screw Trap)"),"GEN"] <- "ToeDrainRST"
-ybus[which(ybus$GEN == "Road 22"),"GEN"] <- "YB_OldRiverRd"
-ybus[which(ybus$GEN == "SBE.u"|ybus$GEN=="SBE.d"),"GEN"] <- "SutterSlough"
-ybus[which(ybus$GEN == "SBGU.2"),"GEN"] <- "BlwGeorgiana"
-ybus[which(ybus$GEN == "Below Lisbon Weir"),"GEN"] <- "YB_LisbonWeir"
-ybus[which(ybus$GEN %in% c("I-80 #1","I-80 #4","I-80 Bridge","I-80 #2","I-80 #7","I-80 #3",
-                           "I-80 #6","I-80 #10","I-80 #11","I-80 #8","I-80 #9","I-80 #5")),"GEN"] <- "I80_yolo"
-## receivers that probably don't matter for analysis
-ybus <- ybus[!ybus$GEN %in% c("2A", "2B", "2C", "2D"),]
-ybus <- ybus[!ybus$GEN %in% c("5D","5A", "5B","5C"),]
-ybus <- ybus[!ybus$GEN %in% c("1A", "1C", "Above Swanstons Rd. Crossing"),]
-## Receivers for which we dont have metadata
-missing <- c("BF.1","BF.2","BF.4","LPS.u","LPS.d")
-ybus <- ybus[!(ybus$GEN %in% missing),]
+setdiff(ybus2$GEN, dist_ybus$Location)
+setdiff(dist_ybus$Location, ybus2$GEN)
 
+ybus2$GEN[ybus2$GEN == "TISDALE"] <- "Tisdale Weir"
+ybus2$GEN[ybus2$GEN == "VERONA"] <- "Verona"
+ybus2$GEN[ybus2$GEN == "TOE DRAIN/TULE CANAL"] <- "Yolo Bypass @ I5"
+ybus2$GEN[ybus2$GEN == "YOLO"] <- "Yolo Bypass @ Fremont Weir"
+ybus2$GEN[ybus2$GEN == "Cache Slough East 1"] <- "YB_BCE"
+ybus2$GEN[ybus2$GEN == "Cache Slough East 2"] <- "YB_BCE2"
+ybus2$GEN[ybus2$GEN == "Cache Slough West 1"] <- "YB_BCW"
+ybus2$GEN[ybus2$GEN == "Cache Slough West 2"] <- "YB_BCW2"
+ybus2$GEN[ybus2$GEN %in% c("I-80 #8", "I-80 #9", "I-80 #7", "I-80 #10", "I-80 #6", "I-80 #2", 
+                           "I-80 #1", "I-80 #11", "I-80 Bridge", "I-80 #5", "I-80 #4", "I-80 #3")] <- "YBUS_I80_180_1"
 
-# make sure all fish are detected at Chipps
-chipps = unique(ybus$FishID[ybus$GEN == "ChippsE"])
-stopifnot(all.equal(chipps, unique(ybus$FishID)))
+ybus2$GEN[ybus2$GEN == "Road 22"] <- "RD22"
+ybus2$GEN[ybus2$GEN == "Base of Toe Drain"] <- "YB_ToeDrain_Base"
+ybus2$GEN[ybus2$GEN == "Below Lisbon Weir"] <- "Lisbon Weir 180kHz"
+ybus2$GEN[ybus2$GEN == "Above Swanstons Rd. Crossing"] <- "Abv_swanston_180"
+ybus2$GEN[ybus2$GEN == "RSTR (Rotary Screw Trap)"] <- "Abv_rstr"
 
-yolo = readRDS("data_clean/YoloAce/yoloace_dfa_detects.rds")
+stopifnot(all(setdiff(ybus2$GEN, dist_ybus$Location)))
+setdiff(dist_ybus$Location, ybus2$GEN) # it's okay that none of these are in the detections records - they are grouped locations, or HR2s from other studies
 
-
-# ending dataframe: FishID", "DateTime_PST", "GEN"
-
-# only include fish with >1 detection
-tapply(ybus$DateTimePST, ybus$FishID)
-
-# check that all movements are in the distance matrices
-
-
-
-if(FALSE){
-## First merge in the new correct rkms
-all_detects <- ybus[order(ybus$FishID, ybus$DetectDate), ] 
-all_detects$prev_FishID <- NA
-all_detects$prev_GEN <- NA
-
-all_detects[, c("prev_FishID", "prev_GEN")] <-
-  as.data.frame(data.table::shift(
-    x = all_detects[, c("FishID", "GEN")],
-    n = 1,
-    fill = NA,
-    type = "lag"
-  ))
-
-first_detects <-
-  all_detects[all_detects$FishID != all_detects$prev_FishID |
-                all_detects$GEN != all_detects$prev_GEN, ] #first detections or first detections at a new receiver
-
-first_detects$movement <-
-  paste(first_detects$prev_GEN, "-", first_detects$GEN, sep = " ")
-
-## removes unneccessary columns from distance matrices to prepare for a join with the detection data
-
-test <- filter(first_detects,Route!="Yolo_Bypass")
-
-test <-
-  merge(
-    test,
-    dist_DCC_Yolo_Tisd_closed[, c("Name", "Total_Length_m")],
-    by.x  = "movement",
-    by.y = "Name",
-    all.x = T
-  )
-
-yolo <- filter(first_detects, Route == "Yolo_Bypass")
-
-yolo <-
-  merge(
-    yolo,
-    dist_DCC_Yolo_Tisd_open[, c("Name", "Total_Length_m")],
-    by.x  = "movement",
-    by.y = "Name",
-    all.x = T
-  )
-
-first_detects <- bind_rows(test, yolo)
-
-first_detects <-
-  first_detects[order(first_detects$FishID, first_detects$DetectDate), ]
-first_detects[which(first_detects$FishID != first_detects$prev_FishID), "Total_Length_m"] <-
-  0
-
-## Manually add in some distances not in matrix
-first_detects[first_detects$movement == "YB_OldRiverRd - I80_yolo", "Total_Length_m"] <- 12880
-first_detects[first_detects$movement == "I80_yolo - YB_LisbonWeir", "Total_Length_m"] <- 10966
-first_detects[first_detects$movement == "I80_yolo - ToeDrainRST", "Total_Length_m"] <- 25396
-first_detects[first_detects$movement == "I80_yolo - ToeDrainBase", "Total_Length_m"] <- 32551
-first_detects[first_detects$movement == "I80_yolo - YB_LibertyIsBase", "Total_Length_m"] <- 40798
-
-first_detects1 <- first_detects %>%
-  group_by(FishID) %>%
-  mutate(cum_length = cumsum(Total_Length_m))
-
-## used to determine with GEN names that didn't match dist matrix names
-unique(first_detects1$movement[!(first_detects1$movement %in% dist_DCC_Yolo_Tisd_closed$Name)])
-df<- first_detects1%>%
- filter(Total_Length_m==0)%>%
-ungroup()%>%
-distinct(movement)
-
-## Formatting for DFA analysis
-str(first_detects1)
-first_detects1$Date<- as_date(ymd_hms(first_detects1$DetectDate))
-first_detects1<- arrange(first_detects1, FishID, DetectDate)#order by tagid and datetime 
-ybus_test<- first_detects1 %>%
-  group_by(FishID) %>%
-  arrange(DetectDate, .by_group = TRUE) %>% # in date/time order
-  #mutate(diff = abs(lag(UpdateRkm, default = first(UpdateRkm))- UpdateRkm)) %>% #adds column for absolute value of distance traveled between detections
-  select(FishID, Date, Total_Length_m) 
-}
+# save cleaned data
+saveRDS(ybus2, "data_clean/YBUS/ybus_clean.rds")
