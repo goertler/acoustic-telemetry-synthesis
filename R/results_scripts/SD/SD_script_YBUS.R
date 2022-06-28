b1069446be99d69871ea7ca494213e8c293fbffd
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2022-06-28 14:02:04 -0700

    make daily matrix addition

--- R/results_scripts/SD/SD_script_YBUS.R
+++ R/results_scripts/SD/SD_script_YBUS.R
@@ -1,11 +1,23 @@
 source("R/01_setup.R")
+library(reshape2)
 
 # estimate variation in travel time (sd)
 
 # load data from results (only one file this time)
-ybus_dpd <- read.csv("results/YBUS/2016_ybus_dpd.csv")
+ybus_dpd <- read.csv("results/YBUS/2016_ybus_dpd_refactor.csv") # latest data 6/28/22
 head(ybus_dpd)
 
+# make matrix
+ybus_dpd$date <- as.Date(ybus_dpd$date_time)
+str(ybus_dpd)
+
+ybus_dpd_matrix <- dcast(summary_ybus_dpd, FishID~date, fun = sum)
+head(ybus_dpd_matrix)
+
+ybus_dpd_matrix[ybus_dpd_matrix == 0] <- NA
+min(ybus_dpd_matrix[,-1], na.rm = TRUE)
+
+# SD
 spread_ybus <- transform(ybus_dpd, SD=apply(ybus_dpd[c(2:ncol(ybus_dpd))],1, sd, na.rm = TRUE))
 rel <- names(spread_ybus[-1])[max.col(!is.na(spread_ybus[-1]), "first")]
 end <- names(spread_ybus[-ncol(spread_ybus)])[max.col(!is.na(spread_ybus[-ncol(spread_ybus)]), "last")]
