74d1631c50637ff6ab13dca3d27befee70c082c7
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2022-08-30 14:46:44 -0700

    updated YBUS SD

--- R/results_scripts/SD/SD_script_YBUS.R
+++ R/results_scripts/SD/SD_script_YBUS.R
@@ -13,14 +13,19 @@ ybus_dpd$date <- as.Date(ybus_dpd$date_time)
 str(ybus_dpd)
 length(unique(ybus_dpd$date))
 
-summary_ybus_dpd <- ybus_dpd %>%
-  group_by(FishID, date) %>%
-  summarise(dist = sum(prop_dist))
+#summary_ybus_dpd <- ybus_dpd %>%
+#  group_by(FishID, date) %>%
+#  summarise(dist = sum(prop_dist))
 
-ybus_dpd_matrix <- dcast(summary_ybus_dpd, FishID~date, fun = sum)
+summary_ybus_dpd <- ybus_dpd %>%
+  group_by(FishID) %>%
+  arrange(date, .by_group = TRUE) %>%
+  complete(date = seq.Date(min(date), max(date), by="days"), fill = list(prop_dist = 0))
+
+ybus_dpd_matrix <- dcast(summary_ybus_dpd, FishID~date, fun = sum, fill = NA_real_)
 head(ybus_dpd_matrix)
 
-ybus_dpd_matrix[ybus_dpd_matrix == 0] <- NA # should double check that this isn't breaking up time series (if zero between > 0 values)
+#ybus_dpd_matrix[ybus_dpd_matrix == 0] <- NA # should double check that this isn't breaking up time series (if zero between > 0 values)
 min(ybus_dpd_matrix[,-1], na.rm = TRUE)
 
 # SD
@@ -39,6 +44,6 @@ sd_dat$end <-  as.Date(gsub('\\.', '-', sd_dat$end))
 
 str(sd_dat)
 sd_dat$Year <- "2016"
-length(unique(sd_dat$FishID))
+length(unique(sd_dat$FishID))#662
 
 write.csv(sd_dat, "results/SD/YBUS.csv")
