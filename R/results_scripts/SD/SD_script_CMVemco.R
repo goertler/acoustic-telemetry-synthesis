57f0ac0dce8f024b9f463c39c4cbb99a26321fd7
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2022-09-02 11:14:22 -0700

    CM vemco sd data

--- R/results_scripts/SD/SD_script_CMVemco.R
+++ R/results_scripts/SD/SD_script_CMVemco.R
@@ -1,58 +1,120 @@
 source("R/01_setup.R")
+library(reshape2)
+library(dplyr)
+library(tidyr)
 
-# estimate variation in travel time (sd)
-# test
-#test <- read.csv("results/CJVemco/2007_CJVemco_dpd.csv")
-#spread_test <- transform(test, SD=apply(test[c(2:ncol(test))],1, sd, na.rm = TRUE))
-#rel <- names(spread_test[-1])[max.col(!is.na(spread_test[-1]), "first")]
-#end <- names(spread_test[-ncol(spread_test)])[max.col(!is.na(spread_test[-ncol(spread_test)]), "last")]
+# data
+cmvemco_dpd <- read.csv("results/CMVemco/cmvemco_dpd_refactor.csv")
+length(unique(cmvemco_dpd$FishID))#296
+cmvemco_dpd$date <- as.Date(cmvemco_dpd$date_time)
+str(cmvemco_dpd)
+length(unique(cmvemco_dpd$date)) #419
 
-# make it reproducible
+key <- read.csv("data/common_data/FishID_key.csv")
+# 2007 2008 2009 2010 2011
+vemco_07 <- subset(key, TagType == "Vemco" & Year == 2007)
+vemco_08 <- subset(key, TagType == "Vemco" & Year == 2008)
+vemco_09 <- subset(key, TagType == "Vemco" & Year == 2009)
+vemco_10 <- subset(key, TagType == "Vemco" & Year == 2010)
+vemco_11 <- subset(key, TagType == "Vemco" & Year == 2011)
 
-# load data from results
-file_names <- list.files(path = "results/CJVemco",recursive = TRUE)
-# need to move into folder to run the rest of the code
-setwd("C:/Users/pgoertler/Desktop/MJ's Repo/merged/acoustic-telemetry-synthesis/results/CJVemco")
+# subset
+vemco_07_dpd <- merge(cmvemco_dpd, vemco_07[,c(2,10)], by = "FishID")
+length(unique(vemco_07_dpd$FishID))#5
 
-for(i in file_names){
-  file <- read.csv(i)
-  }
+vemco_08_dpd <- merge(cmvemco_dpd, vemco_08[,c(2,10)], by = "FishID")
+length(unique(vemco_08_dpd$FishID))#81
 
-#years <- substr(file_names, 1, 4)
+vemco_09_dpd <- merge(cmvemco_dpd, vemco_09[,c(2,10)], by = "FishID")
+length(unique(vemco_09_dpd$FishID))#58
 
-file <- lapply(file_names,read.csv)
-names(file) <- substr(file_names, 1, 4)
-str(file)
+vemco_10_dpd <- merge(cmvemco_dpd, vemco_10[,c(2,10)], by = "FishID")
+length(unique(vemco_10_dpd$FishID))#48
 
-list.names <- substr(file_names, 1, 4)
-output <- vector("list", length(list.names))
-names(output) <- list.names
+vemco_11_dpd <- merge(cmvemco_dpd, vemco_11[,c(2,10)], by = "FishID")
+length(unique(vemco_11_dpd$FishID))#104
 
-dat_goods <- data.frame(FishID = NA, SD = NA, Year = NA, rel = NA, end = NA)
+# order
+vemco_07_summary <- vemco_07_dpd[,c(1,4,5)] %>%
+  group_by(FishID) %>%
+  arrange(date, .by_group = TRUE) %>%
+  complete(date = seq.Date(min(date), max(date), by="days"), fill = list(prop_dist = 0))
 
-# loop
-for (i in substr(file_names, 1, 4)){
-  temp_df <- data.frame(file[[i]])
-  temp_df2 <- transform(temp_df, SD=apply(temp_df[c(2:ncol(temp_df))],1, sd, na.rm = TRUE))
-  temp_df3 <- temp_df2[, c("FishID", "SD")]
-  temp_df3$Year <- i
-  temp_df3$Col_first <- names(temp_df2[-1])[max.col(!is.na(temp_df2[-1]), "first")]
-  temp_df3$rel <- gsub("X","", temp_df3$Col_first)
-  temp_df3$Col_last <- names(temp_df2[-ncol(temp_df2)])[max.col(!is.na(temp_df2[-ncol(temp_df2)]), "last")]
-  temp_df3$end <- gsub("X","", temp_df3$Col_last)
-  dat_goods <- rbind(dat_goods, temp_df3[,-c(4,6)])
-}
+vemco_08_summary <- vemco_08_dpd[,c(1,4,5)] %>%
+  group_by(FishID) %>%
+  arrange(date, .by_group = TRUE) %>%
+  complete(date = seq.Date(min(date), max(date), by="days"), fill = list(prop_dist = 0))
 
+vemco_09_summary <- vemco_09_dpd[,c(1,4,5)] %>%
+  group_by(FishID) %>%
+  arrange(date, .by_group = TRUE) %>%
+  complete(date = seq.Date(min(date), max(date), by="days"), fill = list(prop_dist = 0))
 
-dat_goods <- dat_goods[-1,]
-head(dat_goods)
-str(dat_goods)
-unique(dat_goods$Year)
+vemco_10_summary <- vemco_10_dpd[,c(1,4,5)] %>%
+  group_by(FishID) %>%
+  arrange(date, .by_group = TRUE) %>%
+  complete(date = seq.Date(min(date), max(date), by="days"), fill = list(prop_dist = 0))
 
-# fix date
-dat_goods$rel <-  as.Date(gsub('\\.', '-', dat_goods$rel))
-dat_goods$end <-  as.Date(gsub('\\.', '-', dat_goods$end))
+vemco_11_summary <- vemco_11_dpd[,c(1,4,5)] %>%
+  group_by(FishID) %>%
+  arrange(date, .by_group = TRUE) %>%
+  complete(date = seq.Date(min(date), max(date), by="days"), fill = list(prop_dist = 0))
 
-# back out
-setwd("C:/Users/pgoertler/Desktop/MJ's Repo/merged/acoustic-telemetry-synthesis")
-write.csv(dat_goods, "results/SD/CMVemco.csv")
+# make matrix
+vemco_07_matrix <- dcast(vemco_07_summary, FishID~date, fun = sum, fill = NA_real_)
+vemco_08_matrix <- dcast(vemco_08_summary, FishID~date, fun = sum, fill = NA_real_)
+vemco_09_matrix <- dcast(vemco_09_summary, FishID~date, fun = sum, fill = NA_real_)
+vemco_10_matrix <- dcast(vemco_10_summary, FishID~date, fun = sum, fill = NA_real_)
+vemco_11_matrix <- dcast(vemco_11_summary, FishID~date, fun = sum, fill = NA_real_)
+
+#SD
+spread_vemco_07 <- transform(vemco_07_matrix, SD=apply(vemco_07_matrix[c(2:ncol(vemco_07_matrix))],1, sd, na.rm = TRUE))
+
+rel <- names(vemco_07_matrix[-1])[max.col(!is.na(vemco_07_matrix[-1]), "first")]
+end <- names(vemco_07_matrix[-ncol(vemco_07_matrix)])[max.col(!is.na(vemco_07_matrix[-ncol(vemco_07_matrix)]), "last")]
+sd_vemco_07 <- data.frame(spread_vemco_07[,c(1,31)], rel, end)
+
+head(sd_vemco_07)
+str(sd_vemco_07)
+
+spread_vemco_08 <- transform(vemco_08_matrix, SD=apply(vemco_08_matrix[c(2:ncol(vemco_08_matrix))],1, sd, na.rm = TRUE))
+
+rel <- names(vemco_08_matrix[-1])[max.col(!is.na(vemco_08_matrix[-1]), "first")]
+end <- names(vemco_08_matrix[-ncol(vemco_08_matrix)])[max.col(!is.na(vemco_08_matrix[-ncol(vemco_08_matrix)]), "last")]
+sd_vemco_08 <- data.frame(spread_vemco_08[,c(1,93)], rel, end)
+
+head(sd_vemco_08)
+str(sd_vemco_08)
+
+spread_vemco_09 <- transform(vemco_09_matrix, SD=apply(vemco_09_matrix[c(2:ncol(vemco_09_matrix))],1, sd, na.rm = TRUE))
+
+rel <- names(vemco_09_matrix[-1])[max.col(!is.na(vemco_09_matrix[-1]), "first")]
+end <- names(vemco_09_matrix[-ncol(vemco_09_matrix)])[max.col(!is.na(vemco_09_matrix[-ncol(vemco_09_matrix)]), "last")]
+sd_vemco_09 <- data.frame(spread_vemco_09[,c(1,99)], rel, end)
+
+head(sd_vemco_09)
+str(sd_vemco_09)
+
+spread_vemco_10 <- transform(vemco_10_matrix, SD=apply(vemco_10_matrix[c(2:ncol(vemco_10_matrix))],1, sd, na.rm = TRUE))
+
+rel <- names(vemco_10_matrix[-1])[max.col(!is.na(vemco_10_matrix[-1]), "first")]
+end <- names(vemco_10_matrix[-ncol(vemco_10_matrix)])[max.col(!is.na(vemco_10_matrix[-ncol(vemco_10_matrix)]), "last")]
+sd_vemco_10 <- data.frame(spread_vemco_10[,c(1,122)], rel, end)
+
+head(sd_vemco_10)
+str(sd_vemco_10)
+
+spread_vemco_11 <- transform(vemco_11_matrix, SD=apply(vemco_11_matrix[c(2:ncol(vemco_11_matrix))],1, sd, na.rm = TRUE))
+
+rel <- names(vemco_11_matrix[-1])[max.col(!is.na(vemco_11_matrix[-1]), "first")]
+end <- names(vemco_11_matrix[-ncol(vemco_11_matrix)])[max.col(!is.na(vemco_11_matrix[-ncol(vemco_11_matrix)]), "last")]
+sd_vemco_11 <- data.frame(spread_vemco_11[,c(1,101)], rel, end)
+
+head(sd_vemco_11)
+str(sd_vemco_11)
+
+write.csv(sd_vemco_07, "results/SD/CMvemco_07.csv")
+write.csv(sd_vemco_08, "results/SD/CMvemco_08.csv")
+write.csv(sd_vemco_09, "results/SD/CMvemco_09.csv")
+write.csv(sd_vemco_10, "results/SD/CMvemco_10.csv")
+write.csv(sd_vemco_11, "results/SD/CMvemco_11.csv")
